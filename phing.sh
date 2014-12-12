106ed8bf134467274e64b1d838e0264a51ca2e5b
Maxime <maxoo.design@free.fr>
2014-12-12 15:40:14 +0200

    initialize phing launcher with the shell script

---
+++ phing.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+phing=./bin/phing
+temporaryPhing=./phing.phar
+
+if [ ! -f $phing ]
+then
+    if [ ! -f $temporaryPhing ]
+    then
+        echo ">> download phing.phar from origin"
+        curl http://www.phing.info/get/phing-latest.phar > $temporaryPhing
+    fi
+    
+    echo ">> use $temporaryPhing instead of $phing"
+    phing="php $temporaryPhing"
+    
+elif [ -f $temporaryPhing ]
+then
+    echo ">> remove $temporaryPhing, phing already exists in $phing"
+    rm $temporaryPhing
+fi
+
+$phing "$@"
