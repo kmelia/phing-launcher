90b5709ef082d40d7bdec3cc4826835ba97801e8
Maxime <maxoo.design@free.fr>
2015-01-20 10:02:59 +0200

    improve the install test: - check for file emptiness (phing.phar or bin/phing)

--- tests/phing-launcher/test-install.sh
+++ tests/phing-launcher/test-install.sh
@@ -2,27 +2,44 @@
 
 echo "[start] Test installing and downloading Phing ..."
 
-if [ -f $phingWithPhar ]
+if [ -s $phingWithPhar ]
 then
     echo "Unable to test if $phingWithPhar is already here."
     exitOnFail
 fi
 
-if [ -f $phingWithComposer ]
+if [ -s $phingWithComposer ]
 then
     echo "Unable to test if $phingWithComposer is already here."
     exitOnFail
 fi
 
-sh phing.sh -logger phing.listener.DefaultLogger composer.install 2>&1 >> $logFile
+touch $phingWithPhar
 
 if [ ! -f $phingWithPhar ]
+then
+    echo "Unable to create empty $phingWithPhar file."
+    exitOnFail
+fi
+
+mkdir $(dirname $phingWithComposer)
+touch $phingWithComposer
+
+if [ ! -f $phingWithComposer ]
+then
+    echo "Unable to create empty $phingWithComposer file."
+    exitOnFail
+fi
+
+sh phing.sh -logger phing.listener.DefaultLogger composer.install 2>&1 >> $logFile
+
+if [ ! -s $phingWithPhar ]
 then
     echo "Unable to get $phingWithPhar on curl download."
     exitOnFail
 fi
 
-if [ ! -f $phingWithComposer ]
+if [ ! -s $phingWithComposer ]
 then
     echo "Unable to get $phingWithComposer on Composer."
     exitOnFail
