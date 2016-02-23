1f7a6542aa6a83fd84718c75b547caad73ce2fff
Maxime <maxoo.design@free.fr>
2016-02-23 11:31:29 +0100

    use the exit status code to detect broken phar

--- phing.sh
+++ phing.sh
@@ -85,6 +85,12 @@ then
         rm $phing
     fi
     
+    if ! php $temporaryPhing > /dev/null
+    then
+        showMessage "removing invalid file $temporaryPhing (broken phar)"
+        rm $temporaryPhing
+    fi
+    
     if [ ! -s $temporaryPhing ]
     then
         showMessage "downloading $temporaryPhing from origin"
